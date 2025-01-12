366a81ff6ad651078976d5d3518e1692d6c51bac
yohdev-davidb <david@yohdev.com>
2025-01-12 17:35:34 -0500

    Finished Project 1 week01

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+
+        Console.WriteLine("What is your First Name?");
+        string FirstName = Console.ReadLine();
+
+        Console.WriteLine("What is your last name?");
+        string LastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {LastName}, {FirstName} {LastName}.");
+
     }
 }
