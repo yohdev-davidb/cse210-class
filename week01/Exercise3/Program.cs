98047e53bf0acc0d716e39dbbe428e89aedf11c6
yohdev-davidb <david@yohdev.com>
2025-01-13 23:40:38 -0500

    exersize 3 final

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 11);
+
+
+        Console.WriteLine("Guess the magic number.... ");
+        int userGuess = int.Parse(Console.ReadLine());
+
+        do
+        {
+            if (userGuess > number)
+            {
+
+                Console.WriteLine("Your guess was too high. Try again. ");
+                userGuess = int.Parse(Console.ReadLine());
+            }
+
+
+            else if (userGuess < number)
+            {
+                Console.WriteLine("Your guess was too low. Try again. ");
+                userGuess = int.Parse(Console.ReadLine());
+            }
+
+        } while (userGuess != number);
+
+        Console.WriteLine("Congratulations! You guess the magic number! ");
+
     }
 }
