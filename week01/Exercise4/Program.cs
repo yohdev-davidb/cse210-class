20491703c933a6b00fe373db04896c2680eba56f
yohdev-davidb <david@yohdev.com>
2025-01-17 20:32:42 -0500

    exercize 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,48 @@
 using System;
+using System.ComponentModel;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        
+        List<int> userNumbers = new List<int>();
+        
+        Console.WriteLine("Enter a number to add to the list. Type 0 when you are done. ");
+       
+       int userInputs;
+       userInputs = 0;
+
+       do
+       
+       {
+        userInputs = int.Parse(Console.ReadLine());
+
+        if (userInputs > 0)
+        {
+            userNumbers.Add(userInputs);
+        }
+      
+       } while (userInputs != 0);
+
+    
+        int totalSum = 0;
+
+
+    foreach (int userNumber in userNumbers)
+    {
+        totalSum = (totalSum += userNumber);
     }
+    
+    
+    Console.WriteLine($"The Total Sum is: {totalSum}");
+    
+    
+    
+    }
+
+
+
+
+
 }
