0adab9a1eb0f1404d5dc2741ba1cb8aca4f8048b
yohdev-davidb <david@yohdev.com>
2025-01-26 21:11:26 -0500

    journal

--- week02/Journal/entry.cs
+++ week02/Journal/entry.cs
@@ -3,14 +3,14 @@ using System;
 public class Entry
 {
 
-public string _date;
-public string _promptText;
-public string _entryText;
+    public string _date;
+    public string _promptText;
+    public string _entryText;
 
-public void Display()
-{
-    
-}
+    public void Display()
+    {
+
+    }
 
 
 
