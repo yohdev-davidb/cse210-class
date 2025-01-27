e1eaf8c6fc298b669a7d32d0108f8d3a4204cb64
yohdev-davidb <david@yohdev.com>
2025-01-27 09:56:25 -0500

    journal

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
+
+        Entry journalEntry = new Entry();
+
         PromptGenerator randomPrompt = new PromptGenerator();
         Console.WriteLine(randomPrompt.GetRandomPrompt());
 
