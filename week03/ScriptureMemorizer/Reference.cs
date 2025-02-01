25ee4848e24032a9b9653caa516ce656c68de9f1
yohdev-davidb <david@yohdev.com>
2025-02-01 17:21:25 -0500

    scriptture

---
+++ week03/ScriptureMemorizer/Reference.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class Reference
+{
+    private string _book;
+    private int _chapter;
+    private int _verse;
+    private int _endVerse;
+
+    public Reference(string book, int chapter, int verse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _verse = verse;
+        _endVerse = -1;
+
+    }
+
+    public Reference(string book, int chapter, int startVerse, int endVerse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _verse = startVerse;
+        _endVerse = endVerse;
+
+    }
+
+    public GetDisplayText()
+    {
+        if _endVerse == -1
+        {
+            console.WriteLine($"{_book} {_chapter} {_verse}");
+        }
+
+    }
+
+}
---
+++ week03/ScriptureMemorizer/Word.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class word
+
+private string _text;
+private bool _isHidden;
+
+public void Hide()
+{
+
+    _isHidden = true;
+
+}
+
+public void Show()
+{
+
+    _isHidden = false;
+
+}
